a5258b9105d8280ec60558d827776babda058e11
Tainá Santana <tainnaps@gmail.com>
2024-05-20 03:07:32 -0300

    Remove console.log

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const formulario = document.getElementById("formulario");
-console.log(formulario);
+
 formulario.addEventListener("submit", (evento) => {
   evento.preventDefault();
   alert("Obrigada por entrar em contato!")
